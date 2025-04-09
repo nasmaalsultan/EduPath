6bdcd13cf2b97342f2aa7664cdf2948acd2fa256
nasmaalsultan <nasmasultan318@gmail.com>
2025-04-09 21:51:12 +0400

    adding files

---
+++ Desktop/EduPath/EduPathApp.swift
@@ -0,0 +1,17 @@
+//
+//  EduPathApp.swift
+//  EduPath
+//
+//  Created by Nasma Sultan on 08/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct EduPathApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
